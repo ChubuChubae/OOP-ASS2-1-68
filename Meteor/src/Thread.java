b998d34fb02c1764e9fe4d941f5bf826420f0529
patiphat <119023027+ChubuChubae@users.noreply.github.com>
2025-09-14 13:53:44 +0700

    Create Constructor Thread.java

--- Meteor/src/Thread.java
+++ Meteor/src/Thread.java
@@ -1,4 +1,12 @@
+
+import javax.swing.JLabel;
+
 public class Thread extends java.lang.Thread {
+    JLabel label;
+
+    public Thread(JLabel label) {
+        this.label = label;
+    }
 
     @Override
     public void run() {
