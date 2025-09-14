381381fcc4664c8ac466595b60d2e7f7bf8f8fd3
patiphat <119023027+ChubuChubae@users.noreply.github.com>
2025-09-14 13:38:00 +0700

    Create try catch in Thread.java
    Create Mockup try catch

--- Meteor/src/Thread.java
+++ Meteor/src/Thread.java
@@ -2,6 +2,12 @@ public class Thread extends java.lang.Thread {
 
     @Override
     public void run() {
-
+        while (true) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
